26d33574f6ed4ecfa2a5f3b7af8ab738a8688262
Perham <perham132@hotmail.se>
2019-03-11 12:54:01 +0100

    update

--- src/app/CreateData.java
+++ src/app/CreateData.java
@@ -7,8 +7,7 @@ public class CreateData {
 	public void createSeller() {
 
 		ArrayList<Seller> sellerList = new ArrayList<Seller>();
-		sellerList.add(
-				new Seller("Kalle Li", "076-855632", "Svevägen23", "BMW sold", "online", 1000, 100000, 10, 58, 32, 5));
+		sellerList.add(new Seller("Kalle Li", "076-855632", "Svevägen23", "BMW sold", "online"));
 	}
 
 }
