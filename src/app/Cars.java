d9b86af6c7b254ed7a18bd0a2a014874419f8b64
ArmenAl <armen.al@gmail.com>
2019-03-11 11:34:13 +0100

    add car class

---
+++ src/app/Cars.java
@@ -0,0 +1,68 @@
+package app;
+
+public class Cars {
+	String carUID, brand, name, modell, color, status;
+	int purchasePrice, sellPrice;
+	public Cars(String carUID, String brand, String name, String modell, String color, String status, int purchasePrice,
+			int sellPrice) {
+		
+		this.carUID = carUID;
+		this.brand = brand;
+		this.name = name;
+		this.modell = modell;
+		this.color = color;
+		this.status = status;
+		this.purchasePrice = purchasePrice;
+		this.sellPrice = sellPrice;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public String getCarUID() {
+		return carUID;
+	}
+	public String getColor() {
+		return color;
+	}
+	public String getModell() {
+		return modell;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getPurchasePrice() {
+		return purchasePrice;
+	}
+	public int getSellPrice() {
+		return sellPrice;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public void setCarUID(String carUID) {
+		this.carUID = carUID;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public void setModell(String modell) {
+		this.modell = modell;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPurchasePrice(int purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	public void setSellPrice(int sellPrice) {
+		this.sellPrice = sellPrice;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
