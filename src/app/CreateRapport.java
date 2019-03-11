643e3f97dfc5eea3e6c18908fb5249b9e621d938
ArmenAl <armen.al@gmail.com>
2019-03-11 14:57:50 +0100

    data class

--- src/app/CreateRapport.java
+++ src/app/CreateRapport.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CreateRapport {
@@ -13,21 +14,39 @@ public class CreateRapport {
 				System.out.println("\n======================================");
 				System.out.println("| Skapa rapport.");
 				System.out.println("======================================");
-				System.out.println("| 1. > Skapa rapport.");
+				System.out.println("| 1. > Sålda bilar.");
 				System.out.println("| 2. > Skapa data.");
-				System.out.println("| 9. > Avsluta");
+				System.out.println("| 3. > Skapa data.");
+				System.out.println("| 9. > Gå till huvud meny.");
 				System.out.println("======================================");
 				System.out.print("| Välj från menyn: ");
 				
 				int val = in.nextInt();
 
 				if (val == 1) {
-					
+					System.out.println("\nSålda Bilar\n");
+					readDataFromFile();
 				} else if (val == 2) {
 					
 				} else if (val == 9) {
 					return;
 				} 
 			}
+	}
+	
+	public static void readDataFromFile() {
+		String s;
+		try {
+			s = IOData.readFromFile("soldCars.txt");
+			System.out.println(s);
+			s = IOData.readFromFile("sellerlist.txt");
+			System.out.println(s);
+			s = IOData.readFromFile("carslist.txt");
+			System.out.println(s);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- src/app/IOData.java
+++ src/app/IOData.java
@@ -1,6 +1,8 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,4 +16,16 @@ public class IOData  {
 		}
 		strToFile.close();
 	}
+	
+	public static String readFromFile(String file) throws IOException{
+		String tmpStr;
+		String svar = "";
+		BufferedReader strFromFile = new BufferedReader(new FileReader(file));
+		while ((tmpStr = strFromFile.readLine()) != null) {
+			svar += tmpStr + "\n";
+		}
+		strFromFile.close();
+		return svar;
+	}
+	
 }
