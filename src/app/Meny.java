d94da75a7ba9f4b1cbdf4f3b77b9a61df1cd81a1
xarczuz <chjunchi.chiu@gmail.com>
2019-03-18 15:59:12 +0100

    hej

--- src/app/Meny.java
+++ src/app/Meny.java
@@ -35,18 +35,16 @@ public class Meny {
 				CreateData.createNewTable("TheDataBase.db");
 				
 			} else if (val == 3) {
-	
-				
-					CreateData.insert("Kalle","Li", "076-855632", "Sveavägen 23");
-					CreateData.insert("Björn","Kar", "076-856222", "Brommavägen 5");
-					CreateData.insert("Vlad","Li", "076-895663", "Årstigen 18");
-					CreateData.insert("Van","Mi", "076-66566632", "Öveavägen 73");
-					CreateData.insert("Lenart","Young", "073-875637", "Svantevägen 21");
-					CreateData.insert("Jenny","Davidson", "070-825322", "Fiskhamnen 43");
-					CreateData.insert("Jessica","Lund", "076-775682", "Sundlandvägen 21");
-					CreateData.insert("Johanna","Van", "070-355432", "Kroneberg 42");
-					CreateData.insert("Peter","Li", "070-353632", "Sveavägen 88");
-
+			
+//					CreateData.insert("Kalle","Li", "076-855632", "Sveavägen 23");
+//					CreateData.insert("Björn","Kar", "076-856222", "Brommavägen 5");
+//					CreateData.insert("Vlad","Li", "076-895663", "Årstigen 18");
+//					CreateData.insert("Van","Mi", "076-66566632", "Öveavägen 73");
+//					CreateData.insert("Lenart","Young", "073-875637", "Svantevägen 21");
+//					CreateData.insert("Jenny","Davidson", "070-825322", "Fiskhamnen 43");
+//					CreateData.insert("Jessica","Lund", "076-775682", "Sundlandvägen 21");
+//					CreateData.insert("Johanna","Van", "070-355432", "Kroneberg 42");
+//					CreateData.insert("Peter","Li", "070-353632", "Sveavägen 88");
 		
 			}else if (val == 4) {		
 					CreateData.selectAll("TheDataBase.db");
